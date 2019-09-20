fd956fd396890729936793030df4c0548bbc0efd
Elena Belaya <elena@belaya.ru>
2019-09-20 11:40:24 +0500

    commit

--- views/main.php
+++ views/main.php
@@ -3,23 +3,23 @@
 
 <form method="post" id="period" class="row mt-3">
 	<?php if($period){ ?>
-		<select name="period" class="form-control col-2 ml-3" required>
+		<select name="period" class="form-control ml-3 mt-1 col-xl-2 col-lg-4 col-md-6 col-sm-10" required>
 			<option value="" disabled selected>Выберите период</option>
 			<?php foreach($period as $d){ ?>
 				<option value="<?=$d['year']?>-<?=$d['month']?>"><?= $rus[$d['month']] ?> <?=$d['year']?></option>
 			<?php } ?>
 		</select>
 	<?php } ?>
-	<select name="type" class="form-control col-2 ml-3">
+	<select name="type" class="form-control ml-3 mt-1 col-xl-2 col-lg-4 col-md-6 col-sm-10">
 		<option value="" selected>Выберите тип клиента</option>
 		<option value="0">физ. лицо</option>
 		<option value="1">юр. лицо</option>
 	</select>
-	<button class="btn btn-primary col-1 ml-3">Смотреть</button>
+	<button class="btn btn-primary ml-3 mt-1">Смотреть</button>
 </form>
 
 
-<table class="table table-striped mt-5 d-none" id="report">
+<table class="table table-striped w-100 mt-5 d-none" id="report">
 	<thead>
 		<tr>
 			<th><?= implode('</th><th>',['Услуга','Баланс на начало периода','Приход','Расход','Перерасчет','Итого']); ?></th>
